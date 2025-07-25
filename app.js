dfb0bbe5ce164b1bc214ed3749e7b257a87736f4
Ghulam Qadir <mohammadghulam.qadir@gmail.com>
2025-07-25 17:09:19 +0500

    fix: clean the code

--- app.js
+++ app.js
@@ -2,16 +2,15 @@ import express from 'express';
 import dotenv from 'dotenv';
 import morgan from 'morgan';
 
+import rateLimiter from "./routers/rateLimiter.route.js"
+
 const app = express();
 dotenv.config();
-const PORT = process.env.PORT || 3000;
-const clientConfigs = new Map();
-const requestLogs = new Map();
-import rateLimiter from "./routers/rateLimiter.route.js"
+
 app.use(morgan('dev'));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-app.use('/api/admin', rateLimiter);
+app.use('/', rateLimiter);
 
 export { app }
